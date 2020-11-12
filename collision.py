11ca91aa6f699ad1928eddc22616c6ec170243be
Toadmo <edward.tang2828@gmail.com>
2020-11-11 21:40:35 -0500

    shapes and classes complete

--- collision.py
+++ collision.py
@@ -26,7 +26,14 @@ ball3 = canvas.create_oval(270,270,302,302,fill="blue", tag='ball')
 
 balls = [ball1, ball2, ball3]
 
-shapes = [drawTriangle(400, 475, 200, 350, 400, 150, 'black', 5), drawRectangle(10, 10, 100, 400, 'green', 5), canvas.create_oval(120,120,200,200,fill='yellow',tag='circle')]
+# Stored Coordinates
+tri_coords = [400, 475, 200, 350, 400, 150]
+circ_center = [160, 160]
+
+
+shapes = [canvas.create_polygon(10,10, 10,100, 100,100, 100,10), 
+canvas.create_polygon(tri_coords, outline='#f11', fill='red', width=2), 
+canvas.create_oval(120,120,200,200,fill='yellow',tag='circle')]
 
 speed_x1 = random.randint(-10,10)
 speed_y1 = random.randint(-3,3)
@@ -90,9 +97,43 @@ while True:
 
 # Difficulty with circular and triangular hitboxes
 	
-    if len(canvas.find_overlapping(10, 10, 100, 400)) > 4:
-        print("collision")
+    if len(canvas.find_overlapping(10, 10, 100, 100)) > 1:
+        print("collision square")
+        print(canvas.find_overlapping(0, 0, 500, 500))
+
+# Triangle
+    a = [400, 475]
+    b = [200, 350]
+    c = [400, 150]
+    tri_area = abs((b[0] - a[0])*(c[1] - a[1] - (c[0] - a[0])*(b[1] - a[1])))
+
+    area1 = abs(a[0] - )
+
+    tri_distances = []
+    for x in tri_coords
     
+    function checkTrianglePoint({points,px,py}) {
+  const [point1,point2,point3] = points;
+  const [x1,y1] = point1;
+  const [x2,y2] = point2;
+  const [x3,y3] = point3;
+  //Original gross area
+  const areaOrig = Math.abs( (x2-x1)*(y3-y1) - (x3-x1)*(y2-y1) );
+
+  //Area formed by detection point and triangle vertex
+  const area1 = Math.abs( (x1-px)*(y2-py) - (x2-px)*(y1-py) );
+  const area2 = Math.abs( (x2-px)*(y3-py) - (x3-px)*(y2-py) );
+  const area3 = Math.abs( (x3-px)*(y1-py) - (x1-px)*(y3-py) );
+  const areaTotal = area1 + area2 + area3;
+
+  //Allowable value of calculation error
+  const buffer = 0.1;
+  if (areaTotal >= areaOrig-buffer && areaTotal<= areaOrig+buffer) {
+    return true;
+  }
+  return false;
+}
+
 
 # Instead of touching of first touch, would it be radius distance from center of the ball. 
 
